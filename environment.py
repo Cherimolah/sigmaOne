b07dbc8b6fe709e8d67815513741892f020ff0f2
Cherimolah <inkustregister@mail.ru>
2026-02-28 18:27:05 +0500

    Add some features

--- environment.py
+++ environment.py
@@ -1,9 +1,12 @@
-import copy
+from copy import deepcopy
 import random
-from typing import Optional, List, Set, Tuple
-from enum import Enum
+from typing import Optional, List, Set, Tuple, Iterable
+from enum import IntEnum, Enum
 
-from utils import count_cards
+
+def count_cards(cards: Iterable["Card"]) -> int:
+    count = len(list(filter(lambda x: x is not None, cards)))
+    return count
 
 
 class Number(int):
@@ -58,30 +61,73 @@ class Card:
             return False
         return self.id == other.id
 
+    def __deepcopy__(self, memo):
+        return self
 
-class Step(int):
+    def __le__(self, other):
+        if other is None:
+            return False
+        return self.id <= other.id
 
-    def __repr__(self):
-        if self == 0:
-            return "Player attack"
-        elif self == 1:
-            return "Player defend"
-        else:
-            return super().__repr__()
+    def __ge__(self, other):
+        if other is None:
+            return False
+        return self.id >= other.id
+
+    def __lt__(self, other):
+        if other is None:
+            return False
+        return self.id < other.id
+
+    def __gt__(self, other):
+        if other is None:
+            return False
+        return self.id > other.id
+
+
+all_cards_24 = {Card(x, 9) for x in list(range(24))}
+all_cards_36 = {Card(x, 6) for x in list(range(36))}
+all_cards_52 = {Card(x, 2) for x in list(range(52))}
+
+class Step(Enum):  # Здесь тип Enum, а не  IntEnum потому что так краисивее принтится 🤡
+    PLAYER_ATTACK = 1
+    PLAYER_DEFEND = 2
+    OPPONENT_ATTACK = 3
+    OPPONENT_DEFEND = 4
 
 
 class IllegalStep(Exception):
     pass
 
 
-class ActionType(Enum):
-    ATTACK = 'attack'
-    DEFEND = 'defend'
-    TAKE = 'take'
-    BAT = 'bat'
-    PASS = 'pass'
-    TRANSFER = 'transfer'
-    STOP_ATTACK = 'stop_attack'
+class ActionType(IntEnum):
+    ATTACK = 1
+    TRANSFER = 2
+    DEFEND = 3
+    TAKE = 4
+    BAT = 5
+    PASS = 6
+    STOP_ATTACK = 7
+
+    def __str__(self):
+        if self.value == 1:
+            return 'ATTACK'
+        elif self.value == 2:
+            return 'TRANSFER'
+        elif self.value == 3:
+            return 'DEFEND'
+        elif self.value == 4:
+            return 'TAKE'
+        elif self.value == 5:
+            return 'BAT'
+        elif self.value == 6:
+            return 'PASS'
+        elif self.value == 7:
+            return 'STOP_ATTACK'
+        return ''
+
+
+finished_action_types = (ActionType.TAKE, ActionType.BAT, ActionType.PASS, ActionType.STOP_ATTACK)
 
 
 class Action:
@@ -94,6 +140,17 @@ class Action:
 
     def __str__(self):
         return self.__repr__()
+
+    def __eq__(self, other):
+        if isinstance(other, Action):
+            return self.type == other.type and self.card == other.card
+        return False
+
+    def __hash__(self):
+        return f'{self.type!s}{self.card!s}'.__hash__()
+
+    def to_dict(self):
+        return {'type': self.type.value, 'card': self.card.id}
 
 
 class State:
@@ -115,16 +172,21 @@ class State:
         else:
             offset = 2
         self.offset = offset
-        self.ended = False
-        self.winner = None
+        self.finished = False
+        self.winner: Optional[int] = None
         self.player_take_mode = False
         self.opponent_take_mode = False
-        self.max_card_desk = 5
+        self.max_card_desk = 5  # До первого бита можно подкинуть 5 карт
         self.player_known_cards: Set[Card] = set()
         self.opponent_known_cards: Set[Card] = set()
 
     def load_cards(self):
-        self.deck = [Card(x, self.offset) for x in list(range(self.deck_size))]
+        if self.deck_size == 24:
+            self.deck = list(all_cards_24)
+        elif self.deck_size == 36:
+            self.deck = list(all_cards_36)
+        else:
+            self.deck = list(all_cards_52)
         random.shuffle(self.deck)
         self.trump = self.deck[-1]
         self.player_get_cards()
@@ -148,16 +210,27 @@ class State:
         my_trumps = [x.number for x in self.player_cards if x.suit == self.trump.suit]
         opponent_trumps = [x.number for x in self.opponent_cards if x.suit == self.trump.suit]
         if not my_trumps and not opponent_trumps:
-            self.step = Step(random.randint(1, 2))
+            rand = random.randint(1, 2)
+            if rand == 1:
+                self.step = Step.PLAYER_ATTACK
+            else:
+                self.step = Step.OPPONENT_ATTACK
         elif my_trumps and not opponent_trumps:
-            self.step = Step(0)
+            self.step = Step.PLAYER_ATTACK
         elif not my_trumps and opponent_trumps:
-            self.step = Step(1)
+            self.step = Step.OPPONENT_ATTACK
         else:
-            self.step = Step(min(opponent_trumps) < min(my_trumps))
+            result = min(opponent_trumps) < min(my_trumps)
+            if result:
+                self.step = Step.OPPONENT_ATTACK
+            else:
+                self.step = Step.PLAYER_ATTACK
 
     def attack(self, card: Card):
-        if self.step == 0:
+        if self.step in (Step.PLAYER_DEFEND, Step.OPPONENT_DEFEND):
+            raise IllegalStep('Cannot attack in defend step')
+
+        if self.step == Step.PLAYER_ATTACK:
             hand = self.player_cards
             opponent_hand = self.opponent_cards
         else:
@@ -182,17 +255,24 @@ class State:
         self.desk[1].append(None)
         hand.discard(card)
 
-        if self.step == 0:
+        self.check_win()
+
+        if self.finished:
+            return
+
+        if self.step == Step.PLAYER_ATTACK:
             self.opponent_known_cards.discard(card)
         else:
             self.player_known_cards.discard(card)
 
-        self.check_win()
 
     def defend(self, card: Card, index: Optional[int] = None):
+        if self.step in (Step.PLAYER_ATTACK, Step.OPPONENT_ATTACK):
+            raise IllegalStep('Cannot defend in attack step')
+
         if self.opponent_take_mode or self.player_take_mode:
             raise IllegalStep('You cannot defend while taking mode')
-        if self.step == 0:
+        if self.step == Step.OPPONENT_DEFEND:
             hand = self.opponent_cards
         else:
             hand = self.player_cards
@@ -214,33 +294,46 @@ class State:
             raise IllegalStep("Card is too low")
         self.check_win()
 
-        if self.step == 0:
-            self.player_known_cards.discard(card)
-        else:
+        if self.finished:
+            return
+
+        if self.step == Step.PLAYER_DEFEND:
             self.opponent_known_cards.discard(card)
+        else:
+            self.player_known_cards.discard(card)
+
+        if len(self.desk[0]) == count_cards(self.desk[1]):
+            if self.step == Step.PLAYER_DEFEND:
+                self.step = Step.OPPONENT_ATTACK
+            else:
+                self.step = Step.PLAYER_ATTACK
 
         if len(self.desk[1]) >= self.max_card_desk and all(self.desk[1]):
             self.bat_table()
 
     def bat_table(self):
+        if self.step in (Step.PLAYER_DEFEND, Step.OPPONENT_DEFEND):
+            raise IllegalStep('Cannot bat table in defend step')
         if not (self.desk[0] and self.desk[1] and all(self.desk[1])):
             raise IllegalStep('Not enough cards on table')
 
         cards = {y for x in self.desk for y in x if y}
         self.bat = self.bat.union(cards)
         self.desk = ([], [])
-        if self.step == 0:
+        if self.step == Step.PLAYER_ATTACK:
             self.player_get_cards()
             self.opponent_get_cards()
-            self.step = Step(1)
+            self.step = Step.OPPONENT_ATTACK
         else:
             self.opponent_get_cards()
             self.player_get_cards()
-            self.step = Step(0)
+            self.step = Step.PLAYER_ATTACK
         self.check_win()
         self.max_card_desk = 6
 
     def take(self):
+        if self.step in (Step.PLAYER_ATTACK, Step.OPPONENT_ATTACK):
+            raise IllegalStep('Cannot take in attack step')
         if self.player_take_mode or self.opponent_take_mode:
             raise IllegalStep('Take mode is actually enabled')
 
@@ -250,14 +343,19 @@ class State:
         if None not in self.desk[1]:
             raise IllegalStep('You can take cards only when there are cards not beated')
 
-        if self.step == 0:
+        if self.step == Step.OPPONENT_DEFEND:
             self.opponent_take_mode = True
+            self.step = Step.PLAYER_ATTACK
         else:
             self.player_take_mode = True
+            self.step = Step.OPPONENT_ATTACK
+
 
     def pass_cards(self):
         if not self.opponent_take_mode and not self.player_take_mode:
             raise IllegalStep('Take mode is disabled')
+        if self.step in (Step.PLAYER_DEFEND, Step.OPPONENT_DEFEND):
+            raise IllegalStep('Cannot pass cards in defend step')
         cards = {y for x in self.desk for y in x if y}
 
         if self.player_take_mode:
@@ -266,7 +364,7 @@ class State:
             self.player_known_cards |= cards
 
         self.desk = ([], [])
-        if self.step == 0:
+        if self.step == Step.PLAYER_ATTACK:
             self.opponent_cards = self.opponent_cards.union(cards)
             self.player_get_cards()
         else:
@@ -283,6 +381,9 @@ class State:
         if self.player_take_mode or self.opponent_take_mode:
             raise IllegalStep('Cannot transfer when take mode is enabled')
 
+        if self.step in (Step.PLAYER_ATTACK, Step.OPPONENT_ATTACK):
+            raise IllegalStep('Cannot transfer in attack step')
+
         if not self.desk[0]:
             raise IllegalStep("You can't transfer when none cards sent in attack")
 
@@ -292,12 +393,14 @@ class State:
         if card.number != self.desk[0][0].number:
             raise IllegalStep("Card must be same number to transfer")
 
-        if self.step == 0:
+        if self.step == Step.OPPONENT_DEFEND:
             hand = self.opponent_cards
             opposite_hand = self.player_cards
+            self.player_known_cards.discard(card)
         else:
             hand = self.player_cards
             opposite_hand = self.opponent_cards
+            self.opponent_known_cards.discard(card)
 
         if card not in hand:
             raise IllegalStep('Card must be in hand')
@@ -308,32 +411,47 @@ class State:
         hand.discard(card)
         self.desk[0].append(card)
         self.desk[1].append(None)
-        if self.step == 0:
-            self.step = Step(1)
-        else:
-            self.step = Step(0)
+
         self.check_win()
+        if self.finished:
+            return
+
+        if self.step == Step.OPPONENT_DEFEND:
+            self.step = Step.OPPONENT_ATTACK
+        else:
+            self.step = Step.PLAYER_ATTACK
+
 
     def check_win(self):
         if self.deck:
             return
         if not self.opponent_cards and self.player_cards:
-            self.ended = True
+            self.finished = True
             self.winner = 1
         if not self.player_cards and self.opponent_cards:
-            self.ended = True
+            self.finished = True
             self.winner = 0
 
     def mirror(self) -> "State":
-        state = copy.deepcopy(self)
-        state.player_cards = self.opponent_cards
-        state.opponent_cards = self.player_cards
-        state.player_known_cards = self.opponent_known_cards
-        state.opponent_known_cards = self.player_known_cards
-        if state.step == 0:
-            state.step = Step(1)
+        state = deepcopy(self)
+        state.player_cards = deepcopy(self.opponent_cards)
+        state.opponent_cards = deepcopy(self.player_cards)
+        state.player_known_cards = deepcopy(self.opponent_known_cards)
+        state.opponent_known_cards = deepcopy(self.player_known_cards)
+        if state.step == Step.PLAYER_ATTACK:
+            state.step = Step.OPPONENT_ATTACK
+        elif state.step == Step.PLAYER_DEFEND:
+            state.step = Step.OPPONENT_DEFEND
+        elif state.step == Step.OPPONENT_ATTACK:
+            state.step = Step.PLAYER_ATTACK
         else:
-            state.step = Step(0)
+            state.step = Step.PLAYER_DEFEND
+        if self.player_take_mode:
+            state.opponent_take_mode = True
+            state.player_take_mode = False
+        elif self.opponent_take_mode:
+            state.player_take_mode = True
+            state.opponent_take_mode = False
         return state
 
     def stop_attack(self):
@@ -341,6 +459,14 @@ class State:
             raise IllegalStep('Cannot stop attack while take mode is enabled')
         if count_cards(self.desk[0]) == count_cards(self.desk[1]):
             raise IllegalStep('Cannot stop attack that have not started yet or beated')
+
+        if self.step in (Step.PLAYER_DEFEND, Step.OPPONENT_DEFEND):
+            raise IllegalStep('Cannot stop attack in defend mode')
+
+        if self.step == Step.PLAYER_ATTACK:
+            self.step = Step.OPPONENT_DEFEND
+        elif self.step == Step.OPPONENT_ATTACK:
+            self.step = Step.PLAYER_DEFEND
 
     def next_step(self, action: Action):
         if not action:
@@ -360,14 +486,66 @@ class State:
         elif action.type == ActionType.STOP_ATTACK:
             self.stop_attack()
 
+    def get_available_actions(self) -> list[Action]:
+        """Возвращает список доступных действий"""
+        available_actions = []
+        if self.finished:
+            return available_actions
 
-class Environment:
+        if self.step in (Step.OPPONENT_DEFEND, Step.OPPONENT_ATTACK):
+            hand = self.opponent_cards
+            opponent_hand = self.player_cards
+        else:
+            hand = self.player_cards
+            opponent_hand = self.opponent_cards
 
-    def __init__(self, deck_size: int = 24, transferable: bool = True):
-        assert deck_size in (24, 36, 52)
-        self.state = State(deck_size=deck_size, transferable=transferable)
+        if self.step in (Step.PLAYER_ATTACK, Step.OPPONENT_ATTACK):
+            if self.desk[0]:
+                if len(self.desk[0]) > count_cards(self.desk[1]):
+                    if not self.player_take_mode and not self.opponent_take_mode:
+                        available_actions.append(Action(ActionType.STOP_ATTACK))
+                    else:
+                        available_actions.append(Action(ActionType.PASS))
+                unbeat_cards = [x for i, x in enumerate(self.desk[0]) if self.desk[1][i] is None]
+                if len(self.desk[0]) < self.max_card_desk and len(opponent_hand) > len(unbeat_cards):
+                    desk = self.desk[0] + [x for x in self.desk[1] if x is not None]
+                    numbers = {x.number for x in desk}
+                    cards_available = [x for x in list(hand) if x.number in numbers]
+                    [available_actions.append(Action(ActionType.ATTACK, Card(x.id, self.offset))) for x in
+                     cards_available]
+            else:
+                [available_actions.append(Action(ActionType.ATTACK, Card(x.id, self.offset))) for x in list(hand)]
+            if len(self.desk[0]) != 0 and len(self.desk[0]) == count_cards(self.desk[1]):
+                available_actions.append(Action(ActionType.BAT))
+        else:
+            if self.step == Step.PLAYER_DEFEND and not self.player_take_mode or self.step == Step.OPPONENT_DEFEND and not self.opponent_take_mode:
+                available_actions.append(Action(ActionType.TAKE))
 
-    def reset(self):
-        self.state = State(self.state.deck_size, self.state.transferable)
-        self.state.load_cards()
-        self.state.load_first_step()
+            numbers_hand = {x.number for x in list(hand)}
+            if count_cards(self.desk[1]) == 0 and self.desk[0][0].number in numbers_hand and len(opponent_hand) > len(
+                    self.desk[0]):
+                cards_available = {x for x in list(hand) if x.number == self.desk[0][0].number}
+                [available_actions.append(Action(ActionType.TRANSFER, Card(x.id, self.offset))) for x in
+                 cards_available]
+
+            unbeat_card = None
+            for i, card in enumerate(self.desk[0]):
+                if self.desk[1][i] is None:
+                    unbeat_card = card
+                    break
+            for card in list(hand):
+                if ((unbeat_card.suit == card.suit and card.number > unbeat_card.number) or
+                        (card.suit == self.trump.suit and unbeat_card.suit != self.trump.suit) or
+                        (
+                                unbeat_card.suit == self.trump.suit and card.suit == self.trump.suit and card.number > unbeat_card.number)):
+                    available_actions.append(Action(ActionType.DEFEND, Card(card.id, self.offset)))
+
+        return available_actions
+
+    def __str__(self):
+        return (f'Player cards = {self.player_cards}, opponent cards = {self.opponent_cards}, trump = {self.trump}, desk = {self.desk}, '
+                f'step = {self.step}, deck = {self.deck}, player take mode = {self.player_take_mode}, '
+                f'opponent take mode = {self.opponent_take_mode}, player known cards = {self.player_known_cards}, '
+                f'opponent known cards = {self.opponent_known_cards}')
+
+
--- utils.py
+++ utils.py
@@ -1,22 +1,115 @@
-from typing import Iterable, TYPE_CHECKING
+import copy
+import random
 
-if TYPE_CHECKING:
-    from environment import Card
-
-
-def count_cards(cards: Iterable["Card"]) -> int:
-    i = 0
-    for card in cards:
-        if card:
-            i += 1
-    return i
+from environment import State, all_cards_24, Card, Action, Step
+from tree_search import GameTreeSearcher
+from models import StateModel
+from neural_network import Network
 
 
-def find_top_indices(numbers, top=4):
-    # Создаем список кортежей (индекс, значение)
-    indexed = list(enumerate(numbers))
-    # Сортируем по убыванию значения, затем по возрастанию индекса (для стабильности)
-    indexed.sort(key=lambda x: (-x[1], x[0]))
-    # Берем первые четыре элемента и извлекаем индексы
-    top_four = indexed[:top]
-    return [idx for idx, val in top_four]
+def create_similar_state(state: State, mirror_mode=False) -> State:
+    """
+    Функция создает похожий стейт на основе существующего
+    То есть рандомит все неизвестные достоверно элементы, остальные (известные) копирует как есть
+    :param mirror_mode: если включен, то считает игрока за оппонента
+    :param state:
+    :return:
+    """
+    # Копируем стейт в новый
+    state_copy = State(state.deck_size, state.transferable)
+    state_copy.trump = state.trump
+    state_copy.player_take_mode = state.player_take_mode
+    state_copy.opponent_take_mode = state.opponent_take_mode
+    state_copy.desk = (copy.copy(state.desk[0]), copy.copy(state.desk[1]))
+    state_copy.bat = copy.copy(state.bat)
+
+    pull_cards = all_cards_24
+
+    state_copy.player_known_cards = copy.copy(state.player_known_cards)
+    state_copy.opponent_known_cards = copy.copy(state.opponent_known_cards)
+    state_copy.step = state.step
+    state_copy.finished = state.finished
+    state_copy.max_card_desk = state.max_card_desk
+    state_copy.check_win()
+
+    if not mirror_mode:
+        state_copy.player_cards = copy.copy(state.player_cards)
+        state_copy.opponent_cards = copy.copy(state.player_known_cards)
+    else:
+        state_copy.player_cards = copy.copy(state.opponent_known_cards)
+        state_copy.opponent_cards = copy.copy(state.opponent_cards)
+
+    # Считаем какие карты остались неизвестными
+    pull_cards = pull_cards - state_copy.opponent_cards - state_copy.player_cards - state_copy.bat - set(
+        state_copy.desk[0]) - set(state_copy.desk[1])
+
+    # Если колода была, то убираем из рандома козырь (он переходит в колоду как достоверный)
+    if len(state.deck) > 0:
+        pull_cards = pull_cards - {state_copy.trump}
+        # Далее раскидываем недостающие карты оппоненту и в колоду
+        if not mirror_mode:
+            for _ in range(len(state.opponent_cards) - len(state_copy.opponent_cards)):
+                card = random.choice(list(pull_cards))
+                state_copy.opponent_cards.add(card)
+                pull_cards.discard(card)
+        else:
+            for _ in range(len(state.player_cards) - len(state_copy.player_cards)):
+                card = random.choice(list(pull_cards))
+                state.player_cards.add(card)
+                pull_cards.discard(card)
+        pull_cards = list(pull_cards)
+        random.shuffle(pull_cards)
+        state_copy.deck = pull_cards + [state_copy.trump]
+    # Если колода уже разобрана, то все неизвестные карты уходят оппоненту (в т.ч. и козырь, если он нигде не вычитался)
+    else:
+        if not mirror_mode:
+            state_copy.opponent_cards = state_copy.opponent_cards.union(pull_cards)
+        else:
+            state_copy.player_cards = state_copy.player_cards.union(pull_cards)
+    return state_copy
+
+
+def evaluate_action_tree(model: Network, state: State) -> list[tuple[Action, float]]:
+    """Основная функция для выбора действия"""
+    searcher = GameTreeSearcher(model)
+    return searcher.evaluate_actions(state)
+
+
+def create_state_from_model(model: StateModel) -> State:
+    state = State(deck_size=24, transferable=True)
+    state.player_cards = {Card(x, 9) for x in model.hand}
+    state.opponent_cards = {Card(x, 9) for x in model.known_cards}
+    state.trump = Card(model.trump, 9)
+    state.bat = {Card(x, 9) for x in model.bat}
+    state.opponent_take_mode = model.player_take_mode
+    state.player_take_mode = False
+    state.desk = ([Card(x, 9) for x in model.desk[0]],
+                  [Card(x, 9) for x in model.desk[1] if x])
+    state.finished = False
+    if state.bat:
+        state.max_card_desk = 6
+    else:
+        state.max_card_desk = 5
+
+    pull_cards = (
+            all_cards_24 - state.player_cards - state.opponent_cards - {x for x in state.desk[0]} -
+            {x for x in state.desk[0] if x} - state.bat
+    )
+
+    # Если оставшийся пул карт меньше, чем у оппонента, то рандомим ему карты
+    if model.count_player_cards < len(pull_cards):
+        pull_cards.discard(state.trump)  # Козырь переходит в колоду
+        for _ in range(model.count_player_cards - len(state.opponent_cards)):
+            card = random.choice(list(pull_cards))
+            state.opponent_cards.add(card)
+            pull_cards.discard(card)
+        deck = list(pull_cards)
+        random.shuffle(deck)
+        deck.append(state.trump)
+        state.deck = deck
+    else:
+        state.opponent_cards |= pull_cards
+    state.check_win()
+    state.step = model.step
+
+    return state
